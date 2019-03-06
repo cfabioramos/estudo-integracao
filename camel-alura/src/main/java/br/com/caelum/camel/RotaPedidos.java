736b201546f36561c6ba16c6c8ed336c0230f168
Carlos Fabio <cfabioramos@gmail.com>
2019-03-05 22:42:25 -0300

    Primeiros exercicios

--- camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
+++ camel-alura/src/main/java/br/com/caelum/camel/RotaPedidos.java
@@ -1,6 +1,8 @@
 package br.com.caelum.camel;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.RoutesBuilder;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 
 public class RotaPedidos {
@@ -8,6 +10,24 @@ public class RotaPedidos {
 	public static void main(String[] args) throws Exception {
 
 		CamelContext context = new DefaultCamelContext();
+		context.addRoutes(
+				new RouteBuilder() {
+					@Override
+					public void configure() throws Exception {
+						from("file:pedidos?delay=5s&noop=true")
+								.split().xpath("/pedido/itens/item")
+								.filter().xpath("/item/formato[text()='EBOOK']")
+								.marshal().xmljson()
+								.log("${body}")
+								.setHeader("CamelFileName", simple("${file:name}.json"))
+								.to("file:saida");
+					}
+				}
+		);
+
+		context.start();
+		Thread.sleep(20000);
+		context.stop();
 
 	}	
 }
