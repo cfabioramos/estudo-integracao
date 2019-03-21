aa6c8009e1804e18c2b7b06cd247784ce0da00ea
carlosc <cfabioramos@gmail.com>
2019-03-21 19:41:49 -0300

    Aula do dia 21/03/2019

---
+++ camel-alura/src/main/java/br/com/caelum/camel/SubrotasSoapHttpFromActiveMQ.java
@@ -0,0 +1,65 @@
+package br.com.caelum.camel;
+
+import org.apache.activemq.camel.component.ActiveMQComponent;
+import org.apache.camel.CamelContext;
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class SubrotasSoapHttpFromActiveMQ {
+
+    public static void main(String[] args) throws Exception {
+
+        CamelContext context = new DefaultCamelContext();
+        context.addComponent("activemq", ActiveMQComponent.activeMQComponent("tcp://localhost:61618"));
+        context.addRoutes(new RouteBuilder() {
+
+            @Override
+            public void configure() throws Exception {
+
+                //Outra possibilidade de posicionamento do errorHandler
+                errorHandler(
+                        deadLetterChannel("activemq:queue:pedidos.DLQ").
+                                useOriginalMessage().
+                                logExhaustedMessageHistory(true).
+                                maximumRedeliveries(3).
+                                redeliveryDelay(5000).
+                                onRedelivery((exchange) -> {
+                                    int counter = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_COUNTER);
+                                    int max = (int) exchange.getIn().getHeader(Exchange.REDELIVERY_MAX_COUNTER);
+                                    System.out.println("Redelivery - " + counter + "/" + max);
+                                }));
+
+                from("activemq:queue:pedidos").
+                        routeId("rota-principal").
+                        to("validator:pedido.xsd").
+                        multicast().
+                        to("direct:soap");
+                        //to("direct:http");
+
+                from("direct:soap").
+                        routeId("rota-soap").
+                        to("xslt:pedido-para-soap.xslt").
+                        log("Resultado do Template: ${body}").
+                        to("mock:soap");
+
+                from("direct:http").
+                        routeId("rota-http").
+                        setProperty("pedidoId", xpath("/pedido/id/text()")).
+                        setProperty("email", xpath("/pedido/pagamento/email-titular/text()")).
+                        split().
+                        xpath("/pedido/itens/item").
+                        filter().
+                        xpath("/item/formato[text()='EBOOK']").
+                        setProperty("ebookId", xpath("/item/livro/codigo/text()")).
+                        setHeader(Exchange.HTTP_QUERY,
+                                simple("clienteId=${property.email}&pedidoId=${property.pedidoId}&ebookId=${property.ebookId}")).
+                        to("http4://localhost:8080/webservices/ebook/item");
+            }
+        });
+
+        context.start();
+        Thread.sleep(20000);
+        context.stop();
+    }
+}
