5fc5f0ed2e30c6c935abc80440f2e7a232e67fd1
carlosc <cfabioramos@gmail.com>
2019-03-22 16:44:37 -0300

    Atividades do dia 22/03/2019.

--- camel-spring-boot-projeto/src/main/java/br/com/caelum/camel/Boot.java
+++ camel-spring-boot-projeto/src/main/java/br/com/caelum/camel/Boot.java
@@ -11,20 +11,38 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication
 public class Boot {
 
-	@Autowired
-	private CamelContext context;
+    @Autowired
+    private CamelContext context;
 
-	/**
-	 * Configuração para adicionar o activemq no CamelContext.
-	 * @throws Exception
+    /**
+     * Configuração para adicionar o activemq no CamelContext.
+     *
+     * @throws Exception
+     */
+    @PostConstruct
+    public void init() throws Exception {
+        context.addComponent("activemq", ActiveMQComponent.activeMQComponent("tcp://localhost:61618"));
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot.class, args);
+    }
+
+	/*
+	Uma alternativa caso fosse necessário fazer com que a classe ProdutoService
+	herdasse de outra classe.
+	*/
+    /*
+	@Bean
+    public RoutesBuilder rota() {
+        return new RouteBuilder() {
+            @Override
+            public void configure() throws Exception {
+                from("file:pedidos").
+                to("activemq:queue:pedidos");
+            }
+        };
+    }
 	 */
-	@PostConstruct
-	public void init() throws Exception {
-		context.addComponent("activemq", ActiveMQComponent.activeMQComponent("tcp://localhost:61618"));
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(Boot.class, args);
-	}
 
 }
